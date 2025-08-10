4b2a1fb38a55aafb226f99476db1b29d48aee0c0
katyPlyhauka <pogudinak.e@gmail.com>
2025-08-10 22:36:37 +0300

    homework 12-1-2

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -21,7 +21,8 @@ const themes = [
 
 const HW12 = () => {
     // взять ид темы из редакса
-    const themeId = useSelector<AppStoreType>(state => state.theme)
+    const themeId = useSelector<AppStoreType>(state => state.theme.themeId)
+
 
     const dispatch = useDispatch()
     const change = (id: number) => {// дописать функцию
